92814baacb61ddfdaf6fd17c4a7d3f7532ed0676
dariazahaleanu <daz261@nyu.edu>
2021-11-14 10:56:20 +0100

    updated files

--- project/load_artists_recurrent.py
+++
@@ -1,6 +0,0 @@
-from project.util import get_subcategories_from_category_recurrent, WIKIPEDIA_URL, get_items_from_category
-
-if __name__ == '__main__':
-    # items = get_subcategories_from_category_recurrent("Category:Musical_groups", base_url=WIKIPEDIA_URL)
-    albums = get_subcategories_from_category_recurrent("Category:Albums by artist", WIKIPEDIA_URL)
-    print()
